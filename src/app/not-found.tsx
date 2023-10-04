200f7ee4e72f4c6944c3cac4ee0835f08d3265a9
dLevz <100078678+danlevison@users.noreply.github.com>
2023-10-04 16:20:05 +0100

    refactor: change h2 to h1 + capitalise component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import paperBg from "@/../public/assets/paperbg.jpg"
 import notFoundImg from "@/../public/assets/404.png"
 import Image from "next/image"
 
-const notFound = () => {
+const NotFound = () => {
 	return (
 		<main
 			className="relative flex flex-col justify-center items-center min-h-screen text-center px-8 bg-[#fbf8de]"
@@ -15,16 +15,16 @@ const notFound = () => {
 		>
 			<Image
 				src={notFoundImg}
-				alt="404"
+				alt="Illustration of a snapped penicl with a 404 error code"
 				width={0}
 				height={0}
 				priority
 				style={{ objectFit: "cover" }}
 				className="w-[300px] mt-[-100px] select-none"
 			/>
-			<h2 className="text-5xl md:text-7xl font-bold font-cabinSketch z-10 my-6">
+			<h1 className="text-5xl md:text-7xl font-bold font-cabinSketch z-10 my-6">
 				Oops, I think you&apos;ve got the wrong class!
-			</h2>
+			</h1>
 			<Link
 				href={"/"}
 				className="text-[#5065A8] text-lg md:text-3xl font-bold underline z-10"
@@ -35,4 +35,4 @@ const notFound = () => {
 	)
 }
 
-export default notFound
+export default NotFound
