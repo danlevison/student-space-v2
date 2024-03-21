95b827798911600731c13cc497bd000d0f308a1f
dLevz <100078678+danlevison@users.noreply.github.com>
2024-03-21 13:46:51 +0000

    feat: Use del tag to show strike through on completed task

--- src/components/classroom/studentGrid/StudentGrid.tsx
+++ src/components/classroom/studentGrid/StudentGrid.tsx
@@ -23,104 +23,83 @@ import dogAvatar from "../../../../public/assets/avatars/dog.svg"
 import cheetahAvatar from "../../../../public/assets/avatars/cheetah.svg"
 import lionAvatar from "../../../../public/assets/avatars/lion.svg"
 import otterAvatar from "../../../../public/assets/avatars/otter.svg"
+import ClassTotal from "./ClassTotal"
 
 const StudentGrid = () => {
-	const { studentData } = useContext(StudentDataContext)
-	const [isAddStudentModalOpen, setIsAddStudentModalOpen] = useState(false)
-	const [showConfetti, setShowConfetti] = useState(false)
-	const { width, height } = useWindowSize()
-	const avatars: HTMLImageElement[] = [
-		monkeyAvatar,
-		rabbitAvatar,
-		pandaAvatar,
-		cheetahAvatar,
-		sheepAvatar,
-		chickenAvatar,
-		penguinAvatar,
-		dogAvatar,
-		giraffeAvatar,
-		snakeAvatar,
-		otterAvatar,
-		frogAvatar,
-		lionAvatar
-	]
+  const { studentData } = useContext(StudentDataContext)
+  const [isAddStudentModalOpen, setIsAddStudentModalOpen] = useState(false)
+  const [showConfetti, setShowConfetti] = useState(false)
+  const { width, height } = useWindowSize()
+  const avatars: HTMLImageElement[] = [
+    monkeyAvatar,
+    rabbitAvatar,
+    pandaAvatar,
+    cheetahAvatar,
+    sheepAvatar,
+    chickenAvatar,
+    penguinAvatar,
+    dogAvatar,
+    giraffeAvatar,
+    snakeAvatar,
+    otterAvatar,
+    frogAvatar,
+    lionAvatar,
+  ]
 
-	const handleAddStudentModal = () => {
-		setIsAddStudentModalOpen(true)
-	}
+  const handleAddStudentModal = () => {
+    setIsAddStudentModalOpen(true)
+  }
 
-	const classTotal = studentData.reduce((acc, curr) => {
-		return acc + curr.points
-	}, 0)
-
-	return (
-		<>
-			{studentData.length === 0 ? (
-				<div className="flex flex-col justify-center items-center h-full w-3/4 lg:w-1/2 mx-auto rounded-lg shadow-lg p-4 sm:mt-10 bg-[#f5f5f5]">
-					<h2 className="text-center font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl">
-						Add your students!
-					</h2>
-					<p className="text-center py-8 sm:text-lg">
-						Engage your students by easily tracking and distributing rewards,
-						ensuring that every achievement is acknowledged and celebrated!
-					</p>
-					<button
-						onClick={handleAddStudentModal}
-						className="bg-buttonClr p-3 md:p-4 rounded-lg text-primaryTextClr hover:scale-105 duration-300"
-					>
-						Add students
-					</button>
-				</div>
-			) : (
-				<div className="grid grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4 items-center px-10">
-					<div className="relative flex flex-col justify-center items-center p-8 shadow-lg rounded-md bg-[#f5f5f5]">
-						<p className="font-bold tracking-wide text-center">Class Total</p>
-						<p className="text-center text-primaryTextClr w-[50px] p-2 bg-iconClr rounded-lg mx-auto my-1">
-							{classTotal}
-						</p>
-						<Image
-							src={coinAvatar}
-							alt="Sketched coin"
-							width={55}
-							height={55}
-							className="absolute top-1 left-1"
-							style={{
-								objectFit: "cover"
-							}}
-						/>
-					</div>
-					<StudentCard
-						avatars={avatars}
-						setShowConfetti={setShowConfetti}
-					/>
-					<button
-						onClick={handleAddStudentModal}
-						className="flex flex-col justify-center items-center max-w-[435px] p-[2.40rem] shadow-lg rounded-md bg-[#f5f5f5] hover:scale-105 duration-300"
-					>
-						<p className="text-lg font-bold">Add student</p>
-						<RiAddLine size={30} />
-					</button>
-					{showConfetti && (
-						<Confetti
-							width={width}
-							height={height}
-							gravity={0.05}
-							numberOfPieces={300}
-							className="w-full"
-						/>
-					)}
-				</div>
-			)}
-			{/* Add Student Modal */}
-			{isAddStudentModalOpen && (
-				<AddStudent
-					avatars={avatars}
-					isAddStudentModalOpen={isAddStudentModalOpen}
-					setIsAddStudentModalOpen={setIsAddStudentModalOpen}
-				/>
-			)}
-		</>
-	)
+  return (
+    <>
+      {studentData.length === 0 ? (
+        <div className="flex flex-col justify-center items-center h-full w-3/4 lg:w-1/2 mx-auto rounded-lg shadow-lg p-4 sm:mt-10 bg-[#f5f5f5]">
+          <h2 className="text-center font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl">
+            Add your students!
+          </h2>
+          <p className="text-center py-8 sm:text-lg">
+            Engage your students by easily tracking and distributing rewards,
+            ensuring that every achievement is acknowledged and celebrated!
+          </p>
+          <button
+            onClick={handleAddStudentModal}
+            className="bg-buttonClr p-3 md:p-4 rounded-lg text-primaryTextClr hover:scale-105 duration-300"
+          >
+            Add students
+          </button>
+        </div>
+      ) : (
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4 items-center px-10">
+          <ClassTotal studentData={studentData} />
+          <StudentCard avatars={avatars} setShowConfetti={setShowConfetti} />
+          <button
+            onClick={handleAddStudentModal}
+            className="flex flex-col justify-center items-center max-w-[435px] p-[2.40rem] shadow-lg rounded-md bg-[#f5f5f5] hover:scale-105 duration-300"
+          >
+            <p className="text-lg font-bold">Add student</p>
+            <RiAddLine size={30} />
+          </button>
+          {showConfetti && (
+            <Confetti
+              width={width}
+              height={height}
+              gravity={0.05}
+              numberOfPieces={300}
+              className="w-full"
+            />
+          )}
+        </div>
+      )}
+      {/* Add Student Modal */}
+      {isAddStudentModalOpen && (
+        <AddStudent
+          avatars={avatars}
+          isAddStudentModalOpen={isAddStudentModalOpen}
+          setIsAddStudentModalOpen={setIsAddStudentModalOpen}
+        />
+      )}
+    </>
+  )
 }
 
 export default StudentGrid
