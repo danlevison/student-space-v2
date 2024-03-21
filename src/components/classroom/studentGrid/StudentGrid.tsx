dbbfe5b446b23a85ba8174d40cd4636ee3c75aa8
dLevz <100078678+danlevison@users.noreply.github.com>
2024-03-21 13:12:39 +0000

    feat: Add del tag to strike out task when completed

--- src/components/classroom/studentGrid/StudentGrid.tsx
+++ src/components/classroom/studentGrid/StudentGrid.tsx
@@ -21,84 +21,83 @@ import dogAvatar from "../../../../public/assets/avatars/dog.svg"
 import cheetahAvatar from "../../../../public/assets/avatars/cheetah.svg"
 import lionAvatar from "../../../../public/assets/avatars/lion.svg"
 import otterAvatar from "../../../../public/assets/avatars/otter.svg"
+import ClassTotal from "./ClassTotal"
 
 const StudentGrid = () => {
-	const { studentData } = useContext(StudentDataContext)
-	const [isAddStudentModalOpen, setIsAddStudentModalOpen] = useState(false)
-	const [showConfetti, setShowConfetti] = useState(false)
-	const { width, height } = useWindowSize()
-	const avatars: HTMLImageElement[] = [
-		monkeyAvatar,
-		rabbitAvatar,
-		pandaAvatar,
-		cheetahAvatar,
-		sheepAvatar,
-		chickenAvatar,
-		penguinAvatar,
-		dogAvatar,
-		giraffeAvatar,
-		snakeAvatar,
-		otterAvatar,
-		frogAvatar,
-		lionAvatar
-	]
+  const { studentData } = useContext(StudentDataContext)
+  const [isAddStudentModalOpen, setIsAddStudentModalOpen] = useState(false)
+  const [showConfetti, setShowConfetti] = useState(false)
+  const { width, height } = useWindowSize()
+  const avatars: HTMLImageElement[] = [
+    monkeyAvatar,
+    rabbitAvatar,
+    pandaAvatar,
+    cheetahAvatar,
+    sheepAvatar,
+    chickenAvatar,
+    penguinAvatar,
+    dogAvatar,
+    giraffeAvatar,
+    snakeAvatar,
+    otterAvatar,
+    frogAvatar,
+    lionAvatar,
+  ]
 
-	const handleAddStudentModal = () => {
-		setIsAddStudentModalOpen(true)
-	}
+  const handleAddStudentModal = () => {
+    setIsAddStudentModalOpen(true)
+  }
 
-	return (
-		<>
-			{studentData.length === 0 ? (
-				<div className="flex flex-col justify-center items-center h-full w-3/4 lg:w-1/2 mx-auto rounded-lg shadow-lg p-4 sm:mt-10 bg-[#f5f5f5]">
-					<h2 className="text-center font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl">
-						Add your students!
-					</h2>
-					<p className="text-center py-8 sm:text-lg">
-						Engage your students by easily tracking and distributing rewards,
-						ensuring that every achievement is acknowledged and celebrated!
-					</p>
-					<button
-						onClick={handleAddStudentModal}
-						className="bg-buttonClr p-3 md:p-4 rounded-lg text-primaryTextClr hover:scale-105 duration-300"
-					>
-						Add students
-					</button>
-				</div>
-			) : (
-				<div className="grid grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4 items-center px-10">
-					<StudentCard
-						avatars={avatars}
-						setShowConfetti={setShowConfetti}
-					/>
-					<button
-						onClick={handleAddStudentModal}
-						className="flex flex-col justify-center items-center max-w-[435px] p-[2.40rem] shadow-lg rounded-md bg-[#f5f5f5] hover:scale-105 duration-300"
-					>
-						<p className="text-lg font-bold">Add student</p>
-						<RiAddLine size={30} />
-					</button>
-					{showConfetti && (
-						<Confetti
-							width={width}
-							height={height}
-							gravity={0.05}
-							numberOfPieces={300}
-							className="w-full"
-						/>
-					)}
-				</div>
-			)}
-			{/* Add Student Modal */}
-			{isAddStudentModalOpen && (
-				<AddStudent
-					avatars={avatars}
-					isAddStudentModalOpen={isAddStudentModalOpen}
-					setIsAddStudentModalOpen={setIsAddStudentModalOpen}
-				/>
-			)}
-		</>
-	)
+  return (
+    <>
+      {studentData.length === 0 ? (
+        <div className="flex flex-col justify-center items-center h-full w-3/4 lg:w-1/2 mx-auto rounded-lg shadow-lg p-4 sm:mt-10 bg-[#f5f5f5]">
+          <h2 className="text-center font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl">
+            Add your students!
+          </h2>
+          <p className="text-center py-8 sm:text-lg">
+            Engage your students by easily tracking and distributing rewards,
+            ensuring that every achievement is acknowledged and celebrated!
+          </p>
+          <button
+            onClick={handleAddStudentModal}
+            className="bg-buttonClr p-3 md:p-4 rounded-lg text-primaryTextClr hover:scale-105 duration-300"
+          >
+            Add students
+          </button>
+        </div>
+      ) : (
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4 items-center px-10">
+          <ClassTotal studentData={studentData} />
+          <StudentCard avatars={avatars} setShowConfetti={setShowConfetti} />
+          <button
+            onClick={handleAddStudentModal}
+            className="flex flex-col justify-center items-center max-w-[435px] p-[2.40rem] shadow-lg rounded-md bg-[#f5f5f5] hover:scale-105 duration-300"
+          >
+            <p className="text-lg font-bold">Add student</p>
+            <RiAddLine size={30} />
+          </button>
+          {showConfetti && (
+            <Confetti
+              width={width}
+              height={height}
+              gravity={0.05}
+              numberOfPieces={300}
+              className="w-full"
+            />
+          )}
+        </div>
+      )}
+      {/* Add Student Modal */}
+      {isAddStudentModalOpen && (
+        <AddStudent
+          avatars={avatars}
+          isAddStudentModalOpen={isAddStudentModalOpen}
+          setIsAddStudentModalOpen={setIsAddStudentModalOpen}
+        />
+      )}
+    </>
+  )
 }
 
 export default StudentGrid
--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,10 +1,51 @@
-import React from "react";
-import Scribble from "../Scribble";
-import UserMessage from "./UserMessage";
-import ConditionalNavigationBtn from "./ConditionalNavigationBtn";
-import { landingPageScribbles } from "@/utils/scribbles";
+import React from "react"
+import Scribble from "../Scribble"
+import UserMessage from "./UserMessage"
+import ConditionalNavigationBtn from "./ConditionalNavigationBtn"
 
 const LandingPage = () => {
+  const landingPageScribbles = [
+    {
+      src: "/assets/Scribbles/24.svg",
+      className: "absolute top-10 left-0 w-[75px] md:w-[100px] 2xl:w-[150px]",
+    },
+    {
+      src: "/assets/Scribbles/22.svg",
+      className:
+        "absolute bottom-20 md:bottom-0 right-20 w-[75px] md:w-[100px] 2xl:w-[150px] rotate-12",
+    },
+    {
+      src: "/assets/Scribbles/23.svg",
+      className: "absolute top-32 right-6 w-[50px] md:w-[75px] 2xl:w-[100px]",
+    },
+    {
+      src: "/assets/Scribbles/21.svg",
+      className: "absolute bottom-32 left-16 w-[50px] md:w-[75px]",
+    },
+    {
+      src: "/assets/Scribbles/57.svg",
+      className:
+        "absolute bottom-72 md:bottom-44 2xl:bottom-96 right-7 md:right-56 2xl:right-56 w-[40px] md:w-[50px] 2xl:w-[75px]",
+    },
+    {
+      src: "/assets/Scribbles/53.svg",
+      className: "absolute top-52 md:top-28 2xl:top-32 w-[25px] 2xl:w-[40px]",
+    },
+    {
+      src: "/assets/Scribbles/49.svg",
+      className: "absolute bottom-72 left-10 md:left-24 w-[30px] md:w-[40px]",
+    },
+    {
+      src: "/assets/Scribbles/45.svg",
+      className:
+        "hidden 2xl:block absolute top-72 left-72 w-[30px] 2xl:w-[40px]",
+    },
+    {
+      src: "/assets/Scribbles/50.svg",
+      className: "hidden lg:block absolute bottom-0 w-[100px] 2xl:w-[150px]",
+    },
+  ]
+
   return (
     <section className="flex flex-col justify-center items-center w-full text-center min-h-screen py-24 relative">
       <Scribble scribblesSvgs={landingPageScribbles} />
@@ -22,7 +63,7 @@ const LandingPage = () => {
         <ConditionalNavigationBtn />
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default LandingPage;
+export default LandingPage
